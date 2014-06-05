c9240d2be999119e8f83cc0e9cb89e7b78c13b97
YasminRazavi <yasmin.razavi@gmail.com>
2014-06-05 15:23:28 +0100

    CASHHHHHH

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,3 +1,18 @@
 class Golf
 
+  def self.hole1 n
+     (1..n).map do |i| 
+      m = ""
+      m += 'pling' if i % 3 == 0
+      m += 'plang' if i % 5 == 0
+      m += 'plong' if i % 7 == 0
+      m= m == "" ? i : m
+    end 
+
+  end
+
+  def self.hole2 s
+    s.split.map {|i| i[0]}.join("")
+  end
+
 end
